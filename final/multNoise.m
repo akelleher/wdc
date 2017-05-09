5f52af4861623fd6323573836cebd31ee90a891b
Alex Kelleher <alexpkelleher@gmail.com>
2017-05-09 17:08:03 -0400

    done

--- final/multNoise.m
+++ final/multNoise.m
@@ -3,7 +3,8 @@ function [ h ] = multNoise( length )
 %   Detailed explanation goes here
 
 pd = makedist('Normal');
-h = random(pd, 1, length).'; %Real noise
+normal = random(pd, 1, length).'; %Real noise
+h = abs(normal+1);
 %realNoise = (realNoise / norm(realNoise)) * norm(noiseless) / 10.0^(0.05*SNR); %Scale to proper SNR
 %disp('Adding noise')
 %noisy = realNoise.*noiseless;
